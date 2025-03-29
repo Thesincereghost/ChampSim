b30a7ae8be229d67e56684813f6fb6b1f408d64b
Thesincereghost <vgr976@gmail.com>
2025-03-28 20:38:50 -0500

    remove ghb_with_fdp jobs for now

--- hprc_scripts/submit_all_jobs.sh
+++ hprc_scripts/submit_all_jobs.sh
@@ -38,23 +38,23 @@ sbatch ghb_stride_648.sh
 sbatch ghb_stride_649.sh
 sbatch ghb_stride_654.sh
 sbatch ghb_stride_657.sh
-sbatch ghb_stride_fdp_600.sh
-sbatch ghb_stride_fdp_602.sh
-sbatch ghb_stride_fdp_603.sh
-sbatch ghb_stride_fdp_605.sh
-sbatch ghb_stride_fdp_607.sh
-sbatch ghb_stride_fdp_619.sh
-sbatch ghb_stride_fdp_620.sh
-sbatch ghb_stride_fdp_621.sh
-sbatch ghb_stride_fdp_623.sh
-sbatch ghb_stride_fdp_625.sh
-sbatch ghb_stride_fdp_627.sh
-sbatch ghb_stride_fdp_628.sh
-sbatch ghb_stride_fdp_631.sh
-sbatch ghb_stride_fdp_638.sh
-sbatch ghb_stride_fdp_641.sh
-sbatch ghb_stride_fdp_644.sh
-sbatch ghb_stride_fdp_648.sh
-sbatch ghb_stride_fdp_649.sh
-sbatch ghb_stride_fdp_654.sh
-sbatch ghb_stride_fdp_657.sh
+# sbatch ghb_stride_fdp_600.sh
+# sbatch ghb_stride_fdp_602.sh
+# sbatch ghb_stride_fdp_603.sh
+# sbatch ghb_stride_fdp_605.sh
+# sbatch ghb_stride_fdp_607.sh
+# sbatch ghb_stride_fdp_619.sh
+# sbatch ghb_stride_fdp_620.sh
+# sbatch ghb_stride_fdp_621.sh
+# sbatch ghb_stride_fdp_623.sh
+# sbatch ghb_stride_fdp_625.sh
+# sbatch ghb_stride_fdp_627.sh
+# sbatch ghb_stride_fdp_628.sh
+# sbatch ghb_stride_fdp_631.sh
+# sbatch ghb_stride_fdp_638.sh
+# sbatch ghb_stride_fdp_641.sh
+# sbatch ghb_stride_fdp_644.sh
+# sbatch ghb_stride_fdp_648.sh
+# sbatch ghb_stride_fdp_649.sh
+# sbatch ghb_stride_fdp_654.sh
+# sbatch ghb_stride_fdp_657.sh
